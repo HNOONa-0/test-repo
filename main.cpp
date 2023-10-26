6cb075308cc7cc10d087599d6c0cb4379fd467ca
HNOONa-0 <59091536+HNOONa-0@users.noreply.github.com>
2023-10-26 22:50:13 +0300

    add min function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int getSum(vector<int>& nums){
@@ -7,7 +8,11 @@ int getSum(vector<int>& nums){
     for(int i = 0; i < nums.size(); i++) sum += nums[i];
     return sum;
 }
+int getMin(vector<int>& nums){
+    return *min_element(begin(nums), end(nums) );
+}
 int main(){
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     cout << getSum(nums) << endl;
+    cout << getMin(nums) << endl;
 }
