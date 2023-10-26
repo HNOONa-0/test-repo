a68471e643c6d718c1152e6c479f6fb8f9378cf6
HNOONa-0 <59091536+HNOONa-0@users.noreply.github.com>
2023-10-26 23:01:38 +0300

    commited to main on accident, reseting

--- main.cpp
+++ main.cpp
@@ -8,11 +8,7 @@ int getSum(vector<int>& nums){
     for(int i = 0; i < nums.size(); i++) sum += nums[i];
     return sum;
 }
-int getMin(vector<int>& nums){
-    return *min_element(begin(nums), end(nums) );
-}
 int main(){
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     cout << getSum(nums) << endl;
-    cout << getMin(nums) << endl;
 }
