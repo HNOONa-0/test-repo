d28ae612aee09f430c030f50c3243df2d8d5bb78
HNOONa-0 <59091536+HNOONa-0@users.noreply.github.com>
2023-10-26 21:42:53 +0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int getSum(vector<int>& nums){
+    int sum = 0;
+    for(int i = 0; i < nums.size(); i++) sum += nums[i];
+    return sum;
+}
+int main(){
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    cout << getSum(nums) << endl;
+}
